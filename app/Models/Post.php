037b49e81cd7a05c7b40e482293b79e1ea291469
Keith Jordan <shadow902@gmail.com>
2025-07-29 16:36:03 -0400

    create post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+			'user_id',
+            'title',
+            'body',
+            'price',
+            'is_paid',
+            'visibility',
+		];
+
+    public function profile(): BelongsTo
+    {
+        return $this->belongsto(User::class);
+    }
 }
