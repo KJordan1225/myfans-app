170ed548c4a1bfc447e12c576944507d04b26bfc
Keith Jordan <shadow902@gmail.com>
2025-07-30 18:12:04 -0400

    update creator.php route file

--- routes/creator.php
+++ routes/creator.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CreatorController;
 
 Route::prefix('creator')->middleware(['auth', 'creator'])->group(function () {
-    Route::get('/', fn () => 'Welcome to the Dashboard');
-    Route::get('/stats', fn () => 'Here are your dashboard stats');
+    Route::get('/create}', [CreatorController::class, 'creatorCreatePost'])
+		->name('creator.post.create');
 });
